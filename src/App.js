582542cb5c63c3c6d9700b1a90363539e8f043ea
Frankluisravlo@gmail.com <Frankluisravlo@gmail.com>
2020-06-17 15:27:12 -0400

    Created HiveMain component and Route
    Created HiveMain Component in src/components/HiveMain/HiveMain.js
    Changed Browser Router to HashRouter in index.js
    Changed routes in App.js to handle Home and HiveMain
    
    Co-authored-by: @rscottlundgren, @arkanevision
    
    Close #8

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
+import { Route, Link } from 'react-router-dom'
 import reactLogo from './logo.svg';
 import testLogo from './Hive Logo.png'
-import './App.css';
+import './App.css'
+import HiveMain from './components/HiveMain/HiveMain'
 
 function App() {
-  const [logo, setLogo] = useState(reactLogo)
+  const [logo, setLogo] = useState(testLogo)
 
   function handleChange () {
     if (logo === reactLogo) {
@@ -16,14 +18,22 @@ function App() {
 
   return (
     // remove className to swtich to styled-components
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hive App
-        </p>
+    <div>
+    <Route exact path='/' render={() => (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hive App
+          </p>
         <button onClick={handleChange}>Enter</button>
-      </header>
+        <Link to="/Main">Kane</Link>
+        </header>
+      </div>
+      )} />
+      <Route exact path='/Main' render={() => (
+              <HiveMain />
+      )} />
     </div>
   );
 }
---
+++ src/components/HiveMain/HiveMain.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HiveMain = () => {
+
+  return (
+    <div>
+      <p>hi</p>
+    </div>
+  )
+}
+
+export default HiveMain
